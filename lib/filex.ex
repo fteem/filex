ed634178296ec975d93400fd587ff2f4e6fdb88a
Ilija Eftimov <ileeftimov@gmail.com>
2018-12-17 23:03:23 +0100

    Add Filex.upload/1 and Filex.upload/3

--- lib/filex.ex
+++ lib/filex.ex
@@ -4,15 +4,39 @@ defmodule Filex do
   """
 
   @doc """
-  Hello world.
+  Takes `file_path`, `amount` and `period` as arguments and uploads the file
+  via file.io's API.
+
+  Returns a one-time link to the uploaded file and expiry time.
 
   ## Examples
 
-      iex> Filex.hello()
-      :world
+      Filex.upload("test/fixtures/image.png", 2, :month)
+      { :ok, [url: "https://file.io/aQbnDJ", expires: "60 days"]}
+
+      Filex.upload("nonexistent.png")
+      { :error, "File not found" }
 
   """
-  def hello do
-    :world
+  def upload(file_path, amount, period) do
+    Filex.TimeMapper.map(amount, period)
+    |> Filex.Uploader.upload(file_path)
+    |> Filex.Response.parse()
+  end
+
+  @doc """
+  Helper function for `Filex.upload/3`, defaulting `amount` and `period`
+  to 1 week (7 days).
+
+  ## Examples
+
+      Filex.upload("test/fixtures/image.png")
+      { :ok, [url: "https://file.io/aQbnDJ", expires: "7 days"]}
+
+  """
+  def upload(file_path) do
+    file_path
+    |> Filex.Uploader.upload()
+    |> Filex.Response.parse()
   end
 end
--- test/filex_test.exs
+++ test/filex_test.exs
@@ -1,8 +1,4 @@
 defmodule FilexTest do
   use ExUnit.Case
   doctest Filex
-
-  test "greets the world" do
-    assert Filex.hello() == :world
-  end
 end
