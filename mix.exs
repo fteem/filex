8ab229ba3508a708c6fbc9b29c1e428ad191b867
Ilija Eftimov <ileeftimov@gmail.com>
2018-12-18 01:22:55 +0100

    Add ExCoveralls v0.10.3

--- mix.exs
+++ mix.exs
@@ -7,6 +7,13 @@ defmodule Filex.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
       deps: deps()
     ]
   end
@@ -23,7 +30,8 @@ defmodule Filex.MixProject do
     [
       {:httpoison, "~> 1.5.0"},
       {:poison, "~> 4.0.1"},
-      {:ex_doc, "~> 0.19.1"}
+      {:ex_doc, "~> 0.19.1"},
+      {:excoveralls, "~> 0.10.3", only: :test}
     ]
   end
 end
